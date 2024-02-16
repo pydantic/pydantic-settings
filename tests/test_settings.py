15b66df53b37c29bc943295f87c430db35ac2108
Hasan Ramezani <hasan.r67@gmail.com>
2024-02-16 16:53:55 +0100

    Fix dotenv source extra values parsing provided in dotenv file (#221)

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -839,12 +839,11 @@ def test_env_file_with_env_prefix_invalid(tmp_path):
 
         model_config = SettingsConfigDict(env_file=p, env_prefix='prefix_')
 
-    err_msg = (
-        "unable to load environment variables from dotenv file "
-        "due to the presence of variables without the specified prefix - 'prefix_'"
-    )
-    with pytest.raises(SettingsError, match=err_msg):
+    with pytest.raises(ValidationError) as exc_info:
         Settings()
+    assert exc_info.value.errors(include_url=False) == [
+        {'type': 'extra_forbidden', 'loc': ('f',), 'msg': 'Extra inputs are not permitted', 'input': 'random value'}
+    ]
 
 
 def test_ignore_env_file_with_env_prefix_invalid(tmp_path):
@@ -2310,3 +2309,48 @@ def test_multiple_file_json(tmp_path):
 
     s = Settings()
     assert s.model_dump() == {'json5': 5, 'json6': 6}
+
+
+def test_dotenv_with_alias_and_env_prefix(tmp_path):
+    p = tmp_path / '.env'
+    p.write_text('xxx__foo=1\nxxx__bar=2')
+
+    class Settings(BaseSettings):
+        model_config = SettingsConfigDict(env_file=p, env_prefix='xxx__')
+
+        foo: str = ''
+        bar_alias: str = Field('', validation_alias='xxx__bar')
+
+    s = Settings()
+    assert s.model_dump() == {'foo': '1', 'bar_alias': '2'}
+
+    class Settings1(BaseSettings):
+        model_config = SettingsConfigDict(env_file=p, env_prefix='xxx__')
+
+        foo: str = ''
+        bar_alias: str = Field('', alias='bar')
+
+    with pytest.raises(ValidationError) as exc_info:
+        Settings1()
+    assert exc_info.value.errors(include_url=False) == [
+        {'type': 'extra_forbidden', 'loc': ('xxx__bar',), 'msg': 'Extra inputs are not permitted', 'input': '2'}
+    ]
+
+
+def test_dotenv_with_alias_and_env_prefix_nested(tmp_path):
+    p = tmp_path / '.env'
+    p.write_text('xxx__bar=0\nxxx__nested__a=1\nxxx__nested__b=2')
+
+    class NestedSettings(BaseModel):
+        a: str = 'a'
+        b: str = 'b'
+
+    class Settings(BaseSettings):
+        model_config = SettingsConfigDict(env_prefix='xxx__', env_nested_delimiter='__', env_file=p)
+
+        foo: str = ''
+        bar_alias: str = Field('', alias='xxx__bar')
+        nested_alias: NestedSettings = Field(default_factory=NestedSettings, alias='xxx__nested')
+
+    s = Settings()
+    assert s.model_dump() == {'foo': '', 'bar_alias': '0', 'nested_alias': {'a': '1', 'b': '2'}}
