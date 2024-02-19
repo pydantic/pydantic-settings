ec7f33bc71a4fb8557dcd75ecad5a3ece494a8a2
Hasan Ramezani <hasan.r67@gmail.com>
2024-02-19 18:57:40 +0100

    Revert "Allow nested env var source to override nested init source."

--- pydantic_settings/sources.py
+++ pydantic_settings/sources.py
@@ -8,10 +8,10 @@ from abc import ABC, abstractmethod
 from collections import deque
 from dataclasses import is_dataclass
 from pathlib import Path
-from typing import TYPE_CHECKING, Any, Dict, List, Mapping, Sequence, Tuple, Union, cast
+from typing import TYPE_CHECKING, Any, List, Mapping, Sequence, Tuple, Union, cast
 
 from dotenv import dotenv_values
-from pydantic import AliasChoices, AliasPath, BaseModel, Json, TypeAdapter
+from pydantic import AliasChoices, AliasPath, BaseModel, Json
 from pydantic._internal._typing_extra import WithArgsTypes, origin_is_union
 from pydantic._internal._utils import deep_update, is_model_class, lenient_issubclass
 from pydantic.fields import FieldInfo
@@ -165,7 +165,7 @@ class InitSettingsSource(PydanticBaseSettingsSource):
         return None, '', False
 
     def __call__(self) -> dict[str, Any]:
-        return TypeAdapter(Dict[str, Any]).dump_python(self.init_kwargs, by_alias=True)
+        return self.init_kwargs
 
     def __repr__(self) -> str:
         return f'InitSettingsSource(init_kwargs={self.init_kwargs!r})'
