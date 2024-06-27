d6b693a4c2ca0ee3757d3dd3e054a27dcba25b33
Victor Colomb <victor@tarmactechnologies.com>
2024-06-27 17:39:20 +0200

    fix: type error on python 3.8 (again)

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -3992,7 +3992,7 @@ def test_settings_source_previous_state(env):
         @classmethod
         def settings_customise_sources(
             cls,
-            settings_cls: type[BaseSettings],
+            settings_cls: Type[BaseSettings],
             init_settings: PydanticBaseSettingsSource,
             env_settings: PydanticBaseSettingsSource,
             dotenv_settings: PydanticBaseSettingsSource,
