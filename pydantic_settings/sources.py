6c0402136e094158089873dd8ffe1e3e3ab2650a
Hasan Ramezani <hasan.r67@gmail.com>
2024-08-02 11:35:11 +0330

    Address comment

--- pydantic_settings/sources.py
+++ pydantic_settings/sources.py
@@ -1279,7 +1279,11 @@ class CliSettingsSource(EnvSettingsSource, Generic[T]):
 
     def _sort_arg_fields(self, model: type[BaseModel]) -> list[tuple[str, FieldInfo]]:
         positional_args, subcommand_args, optional_args = [], [], []
-        fields = model.__pydantic_fields__ if is_pydantic_dataclass(model) else model.model_fields
+        fields = (
+            model.__pydantic_fields__
+            if hasattr(model, '__pydantic_fields__') and is_pydantic_dataclass(model)
+            else model.model_fields
+        )
         for field_name, field_info in fields.items():
             if _CliSubCommand in field_info.metadata:
                 if not field_info.is_required():
