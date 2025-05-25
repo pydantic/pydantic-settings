a8c4b0aa4668a45fcbae6bc8d1b6b39f584593c0
Barak <162876273+barakor-vs@users.noreply.github.com>
2025-05-25 17:07:17 +0300

    unformat

--- pydantic_settings/sources/providers/aws.py
+++ pydantic_settings/sources/providers/aws.py
@@ -1,6 +1,4 @@
-from __future__ import (
-    annotations as _annotations,
-)  # important for BaseSettings import to work
+from __future__ import annotations as _annotations  # important for BaseSettings import to work
 
 import json
 from collections.abc import Mapping
@@ -25,7 +23,7 @@ def import_aws_secrets_manager() -> None:
         from mypy_boto3_secretsmanager.client import SecretsManagerClient
     except ImportError as e:  # pragma: no cover
         raise ImportError(
-            "AWS Secrets Manager dependencies are not installed, run `pip install pydantic-settings[aws-secrets-manager]`"
+            'AWS Secrets Manager dependencies are not installed, run `pip install pydantic-settings[aws-secrets-manager]`'
         ) from e
 
 
@@ -50,26 +48,24 @@ class AWSSecretsManagerSettingsSource(EnvSettingsSource):
             settings_cls,
             case_sensitive=True,
             env_prefix=env_prefix,
-            env_nested_delimiter="--",
+            env_nested_delimiter='--',
             env_ignore_empty=False,
             env_parse_none_str=env_parse_none_str,
             env_parse_enums=env_parse_enums,
         )
 
     def _load_env_vars(self) -> Mapping[str, Optional[str]]:
-        response = self._secretsmanager_client.get_secret_value(
-            SecretId=self._secret_id
-        )  # type: ignore
+        response = self._secretsmanager_client.get_secret_value(SecretId=self._secret_id)  # type: ignore
 
-        return json.loads(response["SecretString"])
+        return json.loads(response['SecretString'])
 
     def __repr__(self) -> str:
         return (
-            f"{self.__class__.__name__}(secret_id={self._secret_id!r}, "
-            f"env_nested_delimiter={self.env_nested_delimiter!r})"
+            f'{self.__class__.__name__}(secret_id={self._secret_id!r}, '
+            f'env_nested_delimiter={self.env_nested_delimiter!r})'
         )
 
 
 __all__ = [
-    "AWSSecretsManagerSettingsSource",
+    'AWSSecretsManagerSettingsSource',
 ]
